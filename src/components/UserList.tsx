cf03569af1842338f5846bb5c231e9ad3e55e48e
AndreyOhotnikov <ququruq@gmail.com>
2022-05-17 06:01:14 +0300

    castom hook useDispatch -> useAction

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { useAction } from "../hooks/useAction";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import { fetchUsers } from "../store/action-creators/user";
 
 const UserList: React.FC = () => {
   const {error, loading, users} = useTypedSelector(state => state.user);
-  const dispatch = useDispatch()
+  const {fetchUsers} = useAction()
 
   useEffect(() => {
-    dispatch(fetchUsers())
+    fetchUsers()
   }, [])
   
   
